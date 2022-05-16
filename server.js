0719878ced567a12a3d25751f5f43e9d6d4da9fd
Shai <Shaisisso1@gmail.com>
2022-05-16 18:17:07 +0300

    server.js set routing to index.html

--- server.js
+++ server.js
@@ -26,17 +26,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Step 3
-
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
-// // Step 1:
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 
 // HTTP request logger
