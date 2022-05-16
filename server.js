cd53a1e5902c99d4e699c2c9659203752d97ae0f
Shai <Shaisisso1@gmail.com>
2022-05-16 17:39:10 +0300

    shared heroku

--- server.js
+++ server.js
@@ -31,20 +31,20 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
-let protected = ['transformed.js', 'main.css', 'favicon.ico']
+// let protected = ['transformed.js', 'main.css', 'favicon.ico']
 
-app.get("*", (req, res) => {
+// app.get("*", (req, res) => {
 
-  let path = req.params['0'].substring(1)
+//   let path = req.params['0'].substring(1)
 
-  if (protected.includes(path)) {
-    // Return the actual file
-    res.sendFile(`${__dirname}/build/${path}`);
-  } else {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/build/index.html`);
-  }
-});
+//   if (protected.includes(path)) {
+//     // Return the actual file
+//     res.sendFile(`${__dirname}/build/${path}`);
+//   } else {
+//     // Otherwise, redirect to /build/index.html
+//     res.sendFile(`${__dirname}/build/index.html`);
+//   }
+// });
 
 
 // HTTP request logger
